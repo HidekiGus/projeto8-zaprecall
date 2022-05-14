209f808dae10e1e3b8d2849871421b42940832ae
Gustavo Hideki <gustavohideki@outlook.com>
2022-05-14 06:49:35 -0300

    Finalizado cards respondidos

--- projeto8-zaprecall/src/components/TelaCartas.js
+++ projeto8-zaprecall/src/components/TelaCartas.js
@@ -5,9 +5,16 @@ import logo from "../assets/img/logo-pequeno.png";
 import setinha from "../assets/img/setinha.png";
 import vetor from "../assets/img/Vector.png";
 
-function Carta({ resposta, pergunta, indice }) {
+import laranja from "../assets/img/laranja.png";
+import vermelho from "../assets/img/vermelho.png";
+import verde from "../assets/img/verde.png";
 
+
+function Carta({ resposta, pergunta, indice, aumentarConcluidos }) {
+
+    const [imagem, setImagem] = React.useState();
     const [estado, setEstado] = React.useState("titulo");
+    const [cor, setCor] = React.useState("");
 
     function viraPergunta() {
         setEstado("pergunta");
@@ -17,17 +24,40 @@ function Carta({ resposta, pergunta, indice }) {
         setEstado("resposta");
     }
 
-    function viraRespondido() {
+    function viraRespondidoVermelho() {
         setEstado("respondido");
+        setImagem(vermelho);
+        setCor("vermelho");
+        aumentarConcluidos();
     }
+
+    function viraRespondidoLaranja() {
+        setEstado("respondido");
+        setImagem(laranja);
+        setCor("laranja");
+        aumentarConcluidos();
+    }
+
+    function viraRespondidoVerde() {
+        setEstado("respondido");
+        setImagem(verde);
+        setCor("verde");
+        aumentarConcluidos();
+    }
+
+    function daOi() {
+        console.log("OI")
+    }
+
+
+
 
     return (
         <>
-            <div className="carta recursive">
-                <div className={`titulo ${(estado === "titulo") ? "" : "escondido"}`} onClick={viraPergunta}>
+            <div className="carta recursive ">
+                <div className={`titulo ${(estado === "titulo") ? "" : "escondido"}`} onClick={(estado === "respondido") ? daOi : viraPergunta}>
                     <h1>Pergunta {indice+1}</h1>
                     <img src={vetor}/>
-                    {console.log(pergunta)}
                 </div>
                 <div className={`pergunta ${estado === "pergunta" ? "ativo" : "escondido"}`}>
                     <h1>{pergunta}</h1>
@@ -36,26 +66,27 @@ function Carta({ resposta, pergunta, indice }) {
                 <div className={`resposta ${estado === "resposta" ? "ativo" : "escondido"}`}>
                     <h1>{resposta}</h1>
                     <div className="botoes">
-                        <div className="botao nao-lembrei">
+                        <div className="botao vermelho" onClick={viraRespondidoVermelho}>
                             <h1>Não <br></br>lembrei</h1>
                         </div>
-                        <div className="botao quase-lembrei">
+                        <div className="botao laranja" onClick={viraRespondidoLaranja}>
                             <h1>Quase não lembrei</h1>
                         </div>
-                        <div className="botao zap">
+                        <div className="botao verde" onClick={viraRespondidoVerde}>
                             <h1>Zap!</h1>
                         </div>
                     </div>
                 </div>
-                <div className={`respondido ${estado === "respondida" ? "ativo" : "escondido"}`}>
-                    <img src={setinha} onClick={viraResposta}/>
+                <div className={`respondido ${estado === "respondido" ? "ativo" : "escondido"} ${cor}`}>
+                    <h1>Pergunta {indice+1}</h1>
+                    <img src={imagem}/>
                 </div>
             </div>
         </>
     );
 }
 
-function Cartas() {
+function Cartas({concluidos, setConcluidos, imagemConcluidos, setImagemConcluidos}) {
 
     const testes = [
         ["O que é JSX ?", "Uma extensão de linguagem do JavaScript"],
@@ -82,12 +113,15 @@ function Cartas() {
         perguntasSelecionadas.push(testes[i]);
     }
 
-    console.log("depois:" + perguntasSelecionadas);
+    function aumentarConcluidos(cor) {
+        setConcluidos(concluidos + 1);
+        setImagemConcluidos(imagemConcluidos + cor)
+    }
 
     return (
         <>
             <div className="cartas">
-                {perguntasSelecionadas.map((perguntasSelecionada, index) => <Carta key={index} indice={index} pergunta={perguntasSelecionada[0]} resposta={perguntasSelecionada[1]} />)}
+                {perguntasSelecionadas.map((perguntasSelecionada, index) => <Carta key={index} indice={index} pergunta={perguntasSelecionada[0]} resposta={perguntasSelecionada[1]} aumentarConcluidos={aumentarConcluidos} />)}
             </div>
         </>
     );
@@ -95,6 +129,7 @@ function Cartas() {
 
 export default function TelaCartas({ estado }) {
 
+    const[imagemConcluidos, setImagemConcluidos] = React.useState([]);
     const[concluidos, setConcluidos] = React.useState(0);
     const[totalConcluidos, setTotalConcluidos] = React.useState(4);
 
@@ -105,7 +140,7 @@ export default function TelaCartas({ estado }) {
                     <img src={logo} alt="logo-pequeno" />
                     <h1>ZapRecall</h1>
                 </div>
-                <Cartas />
+                <Cartas concluidos={concluidos} setConcluidos={setConcluidos} imagemConcluidos={imagemConcluidos} setImagemConcluidos={setImagemConcluidos}/>
                 <div className="concluidos recursive">
                     <h1>{concluidos}/{totalConcluidos} CONCLUÍDOS</h1>
                 </div>
