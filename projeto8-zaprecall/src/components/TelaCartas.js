8d575ac488ce6e04a90ff7fdf195da7d2edf1d00
Gustavo Hideki <gustavohideki@outlook.com>
2022-05-14 04:47:37 -0300

    Finalizado layout das respostas com botoes

--- projeto8-zaprecall/src/components/TelaCartas.js
+++ projeto8-zaprecall/src/components/TelaCartas.js
@@ -5,13 +5,20 @@ import logo from "../assets/img/logo-pequeno.png";
 import setinha from "../assets/img/setinha.png";
 import vetor from "../assets/img/Vector.png";
 
-function Carta({ pergunta, indice }) {
+function Carta({ resposta, pergunta, indice }) {
 
     const [estado, setEstado] = React.useState("titulo");
 
     function viraPergunta() {
         setEstado("pergunta");
-        console.log("OII");
+    }
+
+    function viraResposta() {
+        setEstado("resposta");
+    }
+
+    function viraRespondido() {
+        setEstado("respondido");
     }
 
     return (
@@ -19,12 +26,29 @@ function Carta({ pergunta, indice }) {
             <div className="carta recursive">
                 <div className={`titulo ${(estado === "titulo") ? "" : "escondido"}`} onClick={viraPergunta}>
                     <h1>Pergunta {indice+1}</h1>
-                    <img src={vetor} />
+                    <img src={vetor}/>
                     {console.log(pergunta)}
                 </div>
                 <div className={`pergunta ${estado === "pergunta" ? "ativo" : "escondido"}`}>
-                    <h1>PERGUNTA AQUII III IIIII III IIIIIIII III IIIIIIIII IIIIII ?</h1>
-                    <img src={setinha} />
+                    <h1>{pergunta}</h1>
+                    <img src={setinha} onClick={viraResposta}/>
+                </div>
+                <div className={`resposta ${estado === "resposta" ? "ativo" : "escondido"}`}>
+                    <h1>{resposta}</h1>
+                    <div className="botoes">
+                        <div className="botao nao-lembrei">
+                            <h1>Não <br></br>lembrei</h1>
+                        </div>
+                        <div className="botao quase-lembrei">
+                            <h1>Quase não lembrei</h1>
+                        </div>
+                        <div className="botao zap">
+                            <h1>Zap!</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={`respondido ${estado === "respondida" ? "ativo" : "escondido"}`}>
+                    <img src={setinha} onClick={viraResposta}/>
                 </div>
             </div>
         </>
@@ -33,12 +57,37 @@ function Carta({ pergunta, indice }) {
 
 function Cartas() {
 
-    const testes = ["a", "b", "c", "d"];
+    const testes = [
+        ["O que é JSX ?", "Uma extensão de linguagem do JavaScript"],
+        ["O React é __", "uma biblioteca JavaScript para construção de interfaces"],
+        ["Componentes devem iniciar com __", "letra maiúscula"],
+        ["Podemos colocar __ dentro do JSX", "expressões"],
+        ["O ReactDOM nos ajuda __", "interagindo com a DOM para colocar componentes React na mesma"],
+        ["Usamos o npm para __", "gerenciar os pacotes necessários e suas dependências"],
+        ["Usamos props para __", "passar diferentes informações para componentes"],
+        ["Usamos estado (state) para __", "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"]
+    ];
+
+    const perguntas = 4;
+
+    let perguntasSelecionadas = []
+
+    function embaralhar() {
+        return Math.random() - 0.5;
+    }
+
+    testes.sort(embaralhar);
+
+    for (let i=0; i<perguntas; i++) {
+        perguntasSelecionadas.push(testes[i]);
+    }
+
+    console.log("depois:" + perguntasSelecionadas);
 
     return (
         <>
             <div className="cartas">
-                {testes.map((teste, index) => <Carta key={index} indice={index} pergunta={teste} />)}
+                {perguntasSelecionadas.map((perguntasSelecionada, index) => <Carta key={index} indice={index} pergunta={perguntasSelecionada[0]} resposta={perguntasSelecionada[1]} />)}
             </div>
         </>
     );
@@ -47,7 +96,7 @@ function Cartas() {
 export default function TelaCartas({ estado }) {
 
     const[concluidos, setConcluidos] = React.useState(0);
-    const total = 4;
+    const[totalConcluidos, setTotalConcluidos] = React.useState(4);
 
     return (
         <>
@@ -58,7 +107,7 @@ export default function TelaCartas({ estado }) {
                 </div>
                 <Cartas />
                 <div className="concluidos recursive">
-                    <h1>{concluidos}/{total} CONCLUÍDOS</h1>
+                    <h1>{concluidos}/{totalConcluidos} CONCLUÍDOS</h1>
                 </div>
             </div>
         </>
